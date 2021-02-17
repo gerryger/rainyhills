99fb133461e984fa6cdfc4510e69b2dd545e8f8c
Gabriel Caesario <gabrielcaesario@gmail.com>
2021-02-17 19:13:48 +0700

    add swagger and jacoco

--- src/main/java/com/ggunawan/rainyhills/controllers/RainyHillsController.java
+++ src/main/java/com/ggunawan/rainyhills/controllers/RainyHillsController.java
@@ -1,5 +1,34 @@
 package com.ggunawan.rainyhills.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ggunawan.rainyhills.models.HillsRequest;
+import com.ggunawan.rainyhills.models.VolumeResponse;
+import com.ggunawan.rainyhills.services.VolumeCalculatorService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@RestController
+@RequestMapping("/volume-service")
 public class RainyHillsController {
 
+	@Autowired
+	VolumeCalculatorService service;
+	
+	@PostMapping("/calculate")
+	@ApiOperation(value = "Calculating volume of water on hills after rain")
+	@ApiParam(value = "Endpoint to calculate volume of water on hills after rain")
+	public ResponseEntity<VolumeResponse> calculate(@RequestBody HillsRequest request) {
+		Integer volume = service.calculateVolume(request.getHills().stream().mapToInt(i -> i).toArray());
+		VolumeResponse response = new VolumeResponse();
+		response.setVolume(volume);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
 }
--- src/main/java/com/ggunawan/rainyhills/models/HillsRequest.java
+++ src/main/java/com/ggunawan/rainyhills/models/HillsRequest.java
@@ -2,9 +2,11 @@ package com.ggunawan.rainyhills.models;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class HillsRequest {
+	@ApiModelProperty(value = "Hills represented by array of integers", example = "[ 3, 0, 3 ]", dataType = "List")
 	private List<Integer> hills;
 }
